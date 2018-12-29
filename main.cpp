5de83cafa2924dfbdcac4e64734b68a4d4bc7efc
przestaw <pstawczy@mion.elka.pw.edu.pl>
2018-12-29 18:21:13 +0100

    bugfix

--- main.cpp
+++ main.cpp
@@ -2,20 +2,18 @@
 //#include <cstdio>
 
 extern "C" {
-    char output_str[256];
     int func(char *a, char *b);
 }
 
-int main(int argc, char** argv) {
-    char input[256];
-    //printf("Input string: \n");
-    //scanf("%s", &input);
+char output_str[256];
+char input[256];
+
+int main() {
+
     std::cout << "Input string [max 255 char] :\n";
     std::cin >> input;
     func(input, output_str);
     std::cout << "Output string : " << output_str << std::endl;
-
-    //printf("Output string: %s\n", output_str);
 
     return 0;
 }
